22577c4b7738ffaf7c5d258b1e0d2d1d8020a3e6
Xing <zhaoxing8325@gmail.com>
2023-08-06 12:43:31 -0700

    Initial Commit

---
+++ CleanArchitectureAppDemo/CleanArchitectureAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureAppDemoApp.swift
+//  CleanArchitectureAppDemo
+//
+//  Created by XING ZHAO on 2023-08-06.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
